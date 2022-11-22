66336f73fc8e938a78425ff8938152074623a933
KseniiaS <myNew@email.com>
2022-11-22 07:29:48 +0300

    Task 1 raise to the second

--- 1_Lesson/1_0/Program.cs
+++ 1_Lesson/1_0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает число и выдает его квадрат 
+(число умноженное на само себя)
+Например 4 -> 16; -3 -> 9; -7 -> 49;
+*/
+Console.Write("Введите число и оно будет возведено в квадрат - ");
+int digit = int.Parse(Console.ReadLine());
+int result = digit * digit;
+Console.WriteLine( digit + " в квадрате = " + result);
