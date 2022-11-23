85400142c5721a0349f848758e8079a4d7544850
KseniiaS <myNew@email.com>
2022-11-23 07:07:49 +0300

    added_Home_Task

---
+++ 1_Lesson_HomeTask/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 1: Напишите программу, которая на вход принимает два 
+   числа и выдает, какое число большее, а какое меньшее.
+*/
+Console.Write("Введите число 1- ");
+int digit1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2- ");
+int digit2 = int.Parse(Console.ReadLine());
+
+if (digit1 >= digit2){
+    Console.WriteLine("Max = " + digit1);
+}else {
+    Console.WriteLine("Max = " + digit2);
+}
---
+++ 1_Lesson_HomeTask/1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 3: Напишите программу, которая на вход принимает число и выдает, 
+является ли число четным (делится ли оно на два без остатка).
+*/
+Console.Write("Введите число для - ");
+string input  = Console.ReadLine();
+int digit = input == null ? 0 : int.Parse(input);
+
+if ( digit%2 == 0) 
+{
+    Console.WriteLine($"Введенное число {digit} является четным");
+}else
+{
+    Console.WriteLine ($"Число {digit} нечетное");
+}
