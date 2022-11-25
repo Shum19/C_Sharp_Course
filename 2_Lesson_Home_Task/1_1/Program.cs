e52645a91d953154ff5ecc74257810546e570eca
KseniiaS <myNew@email.com>
2022-11-25 09:37:40 +0300

    Adde Home Task 1_1

---
+++ 2_Lesson_Home_Task/1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Задача 1: Напишите программу, которая принимает на вход 
+    трёхзначное число и на выходе показывает вторую цифру этого числа.
+*/
+String ShowSecondDigit(int number)
+{
+    Console.WriteLine(number);
+    
+    if (number < 100 || number > 999)
+    {
+        return "Число не трехзначное";
+    }
+    else 
+    {
+        number /= 10;
+        number %= 10;
+        return $"Второе число - {number}";
+
+    }
+}
+
+Console.Write("Put a number -");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(ShowSecondDigit(number));
