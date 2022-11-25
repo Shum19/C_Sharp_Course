2dd58d555bfc34bde73abc73b94e07140482acc1
KseniiaS <myNew@email.com>
2022-11-25 19:50:10 +0300

    Added Task 1_2

---
+++ 2_Lesson_Home_Task/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*  Задача 2: Напишите программу, которая выводит случайное 
+    трёхзначное число и удаляет вторую цифру этого числа.
+*/
+String JoinFirstLastDigit(int number)
+{
+   
+    Console.WriteLine(number);
+    int firstDigit = number / 100;
+    int lastDigit = number % 10; 
+    return firstDigit.ToString() + lastDigit.ToString();
+}
+int number = new Random().Next(100, 1000);
+Console.WriteLine(JoinFirstLastDigit(number));
