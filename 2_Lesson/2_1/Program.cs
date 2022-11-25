1e7c8428c727852a3da3d4b28092180b5eb51696
KseniiaS <myNew@email.com>
2022-11-25 08:06:16 +0300

    Added Task 2_1

---
+++ 2_Lesson/2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+int ShowLastDigit (int number)
+{
+    Console.WriteLine(number);
+    Console.WriteLine(number %= 10);
+    return number %= 10;
+
+}
+
+
+ShowLastDigit(new Random().Next(10, 1000));
