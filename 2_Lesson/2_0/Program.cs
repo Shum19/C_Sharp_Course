b027fbd10d9d6082d4989d56d29b4c082c92d62b
KseniiaS <myNew@email.com>
2022-11-25 07:32:06 +0300

    added Tast 2_0 from practice

---
+++ 2_Lesson/2_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка 
+//  [10, 99] и показывает наибольшую цифру числа.
+ 
+ int MaxDigit (int num)
+ {
+    Console.WriteLine(num);
+    if (num % 10 > num / 10)
+        return num % 10;
+    else 
+        return num / 10;
+ }
+Console.WriteLine(MaxDigit(new Random().Next(1,100)));
