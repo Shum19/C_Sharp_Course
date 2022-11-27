f6b1d5653465d4bc09855ca98892b4870d8bdc2b
KseniiaS <myNew@email.com>
2022-11-27 14:03:03 +0300

    Added Task 2_3

---
+++ 2_Lesson_Home_Task/2_3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  Напишите программу, которая выводит третью цифру 
+    заданного числа или сообщает, что третьей цифры нет.
+*/ 
+String ShowThirdDigit (long number)
+{    
+    if (number > 999) 
+    {   
+        long i = number;
+        while( i > 1000)
+        {
+            number /= 10;
+            i /=10;
+        }
+    }
+    else if (number > 99 && number < 1000)
+    {
+        number %= 10;
+        return $"The third digit is {number}";
+    }
+    else if (number < 100) 
+    {
+        return "Threre is no third digit!";
+    }
+    number %= 10;
+    return $"A third digit of big number is {number}!";
+}
+
+
+Console.Write("Put a number and find a third digit ");
+long number = long.Parse(Console.ReadLine());
+Console.WriteLine(ShowThirdDigit(number));
+
+
+
