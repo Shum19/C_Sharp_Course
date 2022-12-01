89f2d605b8ef7d3c219936492fe1b7f96fe6c43e
KseniiaS <myNew@email.com>
2022-12-01 07:19:03 +0300

    Adde folder Lesson_Home_Task&Tasks 3_1 and 3_3

---
+++ 3_Lesson_Home_Task/3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*  Задача 1: Напишите программу, которая принимает на вход пятизначное 
+    число и проверяет, является ли оно палиндромом.
+    14212 -> нет 
+    23432 -> да 
+    12821 -> да
+*/
+
+int InputData(string message)
+{
+    Console.Write(message);
+    string text = Console.ReadLine();
+    int number = text == null ? 0 : int.Parse(text);
+    return number;
+}
+
+String isNumberPalindrome (int digit)
+{
+    int firstDigit = digit / 10000;
+    int secondDigit = digit / 1000 % 10;
+    int forthDigit = digit / 10 %10;
+    int fifthDigit = digit % 10;
+    if ( firstDigit == fifthDigit && secondDigit == forthDigit)
+    {
+        return $"{digit} is a Palindrome";
+    }
+    else
+        return $"{digit} is NOT a Palindrome"; 
+}
+
+int test = InputData("Write a five digit number ");
+Console.Write(isNumberPalindrome(test));
+
---
+++ 3_Lesson_Home_Task/3_3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*  Задача 3: Напишите программу, которая принимает на вход число (N) и 
+    выдаёт таблицу кубов чисел от 1 до N. 
+    3 -> 1, 8, 27.
+    5 -> 1, 8, 27, 64, 125
+*/ 
+int InputData(string message)
+{
+    Console.Write(message);
+    string text = Console.ReadLine();
+    int number = text == null ? 0 : int.Parse(text);
+    return number;
+}
+
+void TableOfCubic (int digit) 
+{
+    int i = 1;
+    double table = 0;
+    while (i <= digit)
+    {
+        if (i == digit)
+        {
+            table = Math.Pow (i, 3);
+            Console.Write($"{table}.");
+        }
+        else 
+        {
+            table = Math.Pow(i, 3);
+            Console.Write($"{table}, ");
+        }
+        i++;
+    }
+}
+
+int test = InputData("Write a digit ");
+TableOfCubic(test);
+
