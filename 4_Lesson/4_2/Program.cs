31c059b0c67c0de3c446fc33ee93375e970a3996
KseniiaS <myNew@email.com>
2022-12-03 09:19:10 +0300

    Added Practice of Lesson 4

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*  Задача 1: Напишите программу, которая принимает на вход число 
+    и выдаёт количество цифр в числе.
+    456 -> 3 
+    78 -> 2 
+    89126 -> 5
+*/
+int InputDate(string message)
+{
+    Console.Write(message);
+    string text = Console.ReadLine();
+    int digit = text == null ? 0 : int.Parse(text);
+    return digit;
+}
+void Quantities(int number)
+{
+    if (number == 0) Console.Write(1);
+    else
+    {
+        int i = 0;
+        if (number < 0)
+        {
+            number = number * (-1);
+            while (number > 0)
+            {
+                number /= 10;
+                i++;
+            }
+        }
+        else
+        {
+            while (number > 0)
+            {
+                number /= 10;
+                i++;
+            }
+        }
+        Console.WriteLine(i);
+    }
+}
+int test = InputDate("Write number ");
+Console.WriteLine();
+Quantities(test);
---
+++ 4_Lesson/4_4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*  Задача 3: Напишите программу, которая выводит массив из 8 элементов, 
+    заполненный нулями и единицами в случайном порядке.
+    [1,0,1,1,0,1,0,0] 
+*/int InputDate(string msg)
+{
+    Console.Write(msg);
+    string text = Console.ReadLine();
+    int digit = text == null ? 0 : int.Parse(text);
+    return digit;
+}
+void ArrayPrint(int numOfElements)
+{
+    int [] arr = new int [numOfElements];
+    int size = arr.Length -1;
+    for (int i = 0; i <= size; i++)
+    {
+        arr [i] = new Random().Next(0, 2);
+        Console.Write($"{arr[i]}, ");
+    }
+}
+int test = InputDate("Write num of elements ");
+Console.WriteLine();
+ArrayPrint(test);
