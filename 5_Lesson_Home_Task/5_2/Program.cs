e4ab8064b8627ab4269f47c9fbc4dd8f61bc4456
KseniiaS <myNew@email.com>
2022-12-08 07:13:35 +0300

    Added Home_Task_5_2

---
+++ 5_Lesson_Home_Task/5_2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+    Найдите сумму элементов, стоящих на нечётных позициях.
+    [3, 7, 23, 12] -> 26
+    [-4, -6, 4, 6] -> 0
+  
+*/
+Console.Clear();
+int [] CreatingArray (int size)
+{
+    int [] arr = new int [size];
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr [i] = new Random().Next(-100, 101);
+        Console.Write($"{arr [i]} ");
+    }
+    Console.Write("] - > ");
+    return arr;
+}
+
+void OddPositionSum(int [] array)
+{
+    int size = array.Length;
+    int sum = 0;
+    for (int i = 0; i < size; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    Console.Write(sum);
+}
+
+
+int [] test = CreatingArray (4);
+OddPositionSum(test);
