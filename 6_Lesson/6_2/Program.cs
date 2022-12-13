aca5775b51a049064ef306ba3c1d8555a6fae384
KseniiaS <myNew@email.com>
2022-12-13 12:20:00 +0300

    Added branch 6_Lesson and practice

---
+++ 6_Lesson/6_2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*  Задача 1: Напишите программу, которая принимает на вход три числа и проверяет,
+    может ли существовать треугольник с сторонами такой длины.
+    Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+Console.Clear();
+Console.Write("Input number 3 first - ");
+int [] CreatArry (int size)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input side of triangle {i+1} - ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+Boolean CanBeTriangle (int [] array)
+{
+    int a = array[0];
+    int b = array [1];
+    int c = array [2];
+    if (a < b + c & b < a + c & c < a + b) return true;
+    return false;
+}
+
+int [] test = CreatArry(int.Parse(Console.ReadLine()));
+PrintArray(test);
+Console.WriteLine(CanBeTriangle(test));
---
+++ 6_Lesson/6_4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  
+    Задача 3: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+    Первые два числа Фибоначчи: 0 и 1.
+    Если N = 5 -> 0 1 1 2 3 
+    Если N = 3 -> 0 1 1
+    Если N = 7 -> 0 1 1 2 3 5 8
+*/
+Console.Clear();
+void Fibbonacci( int number)
+{
+    int a = 0, b = 1;
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"{a} ");
+        (a, b) = (b, a+b);
+    }
+}
+Fibbonacci(int.Parse(Console.ReadLine()));
