e16165b6125fc7db96d35de5a318eaca6be466b8
KseniiaS <myNew@email.com>
2023-01-04 15:09:17 +0300

    Added Very Hard to Understand recursion HomeTask

---
+++ 9_Lesson_Home_Task/9_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*  Задача 1: Задайте значения M и N. Напишите программу, 
+    которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+    M = 1; N = 5 -> "2, 4"
+    M = 4; N = 8 -> "4, 6, 8"
+*/
+Console.Clear();
+void EvenNumbersFromTo (int from, int to)
+{
+    if (from > to) return;
+    if (from % 2 != 0) from = from + 1;
+    Console.Write($"{from}, ");
+    EvenNumbersFromTo(from+2, to);
+}
+EvenNumbersFromTo(-10, 10);
