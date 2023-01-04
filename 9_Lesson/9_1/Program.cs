fd6db529f0866253e3777ee68ce6275d62555366
KseniiaS <myNew@email.com>
2023-01-04 14:32:15 +0300

    Added branch 9_Lesson and 9_Lesson/9_1/9_2/9_3/9_4

---
+++ 9_Lesson/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*  Задача 1: Задайте значение N. Напишите программу, 
+    которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+    N = 5 -> "1, 2, 3, 4, 5"
+    N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+Console.Clear();
+void PrintNumbersTo (int n)
+{
+    if (n == 0 ) return;
+    PrintNumbersFromTo(n-1);
+    Console.Write($"{n} ");
+}
+PrintNumbersFromTo(5);
---
+++ 9_Lesson/9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*  Задача 3: Напишите программу, которая будет принимать на вход 
+    число и возвращать сумму его цифр. Использовать рекурсию.
+    453 -> 12   
+    45 -> 9
+*/
+Console.Clear();
+int SumOfNumber (int digit)
+{
+    if (digit == 0) return 0;    
+    return digit%10 + SumOfNumber(digit/10);
+}
+Console.WriteLine(SumOfNumber(453));
